c5b5b19d7c72318f966e220d404c55152751992f
nvhiii <nahikhan16@gmail.com>
2023-08-14 17:47:43 -0400

    register insertion from html form works

--- final_project/register.php
+++ final_project/register.php
@@ -18,10 +18,8 @@
 		die ("Connection failed: " . $conn->connect_error);
 	
 	}
-	
-	// Assign php var to html form vals for 'personal' table gotten via POST
 
-	// these values all use the name attr
+	// these values all use the name attr in html form
 
 	$lnm = $_POST["lastName"];
 	$fnm = $_POST["firstName"];
@@ -32,20 +30,17 @@
 	
 	$ag = $_POST["age"];
 	
-	$sq = $_POST["answerSQ"];
+	$asq = $_POST["answerSQ"];
 	
-	$sql = INSERT INTO personal (lastName, firstName, email, gender, age, security) VALUES ("$lnm", "$fnm", "$em", "$g", "$ag", "$sq");
-	
-	header("location: http://localhost:8888/final_project/success.html");
+	$conn->query("INSERT INTO personal (lastName, firstName, email, gender, age, security) VALUES ('$lnm', '$fnm', '$em', '$g', '$ag', '$asq')");
 	
 	// The below is user/pwd, necessary for future login.
 	
-	/*
-	
 	$un = $_POST["uname"];
+	
 	$pass = $_POST["pwd"];
 	
-	$sql = INSERT INTO login (Username, Password) VALUES ('$un', '$pass');
+	$conn->query("INSERT INTO login (Username, Password) VALUES ('$un', '$pass')");
 	
-	*/
+	
 ?>
